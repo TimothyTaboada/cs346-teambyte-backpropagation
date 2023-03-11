20a99700533f15e7850c406af79a205de5260e1a
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-03-12 00:47:04 +0800

    Added CSVHelper dependency

--- BackPropagation/Form1.cs
+++ BackPropagation/Form1.cs
@@ -7,11 +7,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Backprop;
 
 namespace BackPropagation
 {
     public partial class Form1 : Form
     {
+        NeuralNet neuro = new NeuralNet(4, 10, 1);
         public Form1()
         {
             InitializeComponent();
