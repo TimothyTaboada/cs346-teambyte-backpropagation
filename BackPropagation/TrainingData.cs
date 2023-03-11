456f91df683c18a3534788eea0e53af7c5f2196c
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-03-12 01:42:24 +0800

    Added TrainingData class

---
+++ BackPropagation/TrainingData.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackPropagation
+{
+    internal class TrainingData
+    {
+        public double variance { get; set; }
+        public double skewness { get; set; }
+        public double curtosis { get; set; }
+        public double entropy { get; set; }
+        public int classification { get; set; }
+    }
+}
