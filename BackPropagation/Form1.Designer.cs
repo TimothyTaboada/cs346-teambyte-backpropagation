e644a663f944b5eef9959760c08bad5457594e5f
Timothy Janssen Taboada <timothyjanssen.taboada@cit.edu>
2023-03-12 02:14:42 +0800

    Implemented Training

--- BackPropagation/Form1.Designer.cs
+++ BackPropagation/Form1.Designer.cs
@@ -28,13 +28,128 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.trainbtn = new System.Windows.Forms.Button();
+            this.trainepochin = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.clearbtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.LightGreen;
+            this.pictureBox1.Location = new System.Drawing.Point(373, -2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(21, 453);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.LightGreen;
+            this.pictureBox2.Location = new System.Drawing.Point(-2, 316);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(377, 21);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.LightGreen;
+            this.pictureBox3.Location = new System.Drawing.Point(-2, -2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(377, 45);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.LightGreen;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(81, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(219, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Fake Money Identifier";
+            // 
+            // trainbtn
+            // 
+            this.trainbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.trainbtn.Location = new System.Drawing.Point(415, 119);
+            this.trainbtn.Name = "trainbtn";
+            this.trainbtn.Size = new System.Drawing.Size(235, 35);
+            this.trainbtn.TabIndex = 4;
+            this.trainbtn.Text = "Train";
+            this.trainbtn.UseVisualStyleBackColor = true;
+            this.trainbtn.Click += new System.EventHandler(this.trainbtn_Click);
+            // 
+            // trainepochin
+            // 
+            this.trainepochin.Location = new System.Drawing.Point(415, 77);
+            this.trainepochin.Name = "trainepochin";
+            this.trainepochin.Size = new System.Drawing.Size(235, 20);
+            this.trainepochin.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(416, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(234, 24);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Enter # of Training Epochs";
+            // 
+            // clearbtn
+            // 
+            this.clearbtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearbtn.Location = new System.Drawing.Point(415, 377);
+            this.clearbtn.Name = "clearbtn";
+            this.clearbtn.Size = new System.Drawing.Size(235, 35);
+            this.clearbtn.TabIndex = 7;
+            this.clearbtn.Text = "Clear";
+            this.clearbtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(678, 450);
+            this.Controls.Add(this.clearbtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.trainepochin);
+            this.Controls.Add(this.trainbtn);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button trainbtn;
+        private System.Windows.Forms.TextBox trainepochin;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button clearbtn;
     }
 }
 
--- BackPropagation/Form1.cs
+++ BackPropagation/Form1.cs
@@ -3,20 +3,50 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Backprop;
+using CsvHelper;
+using CsvHelper.Configuration;
 
 namespace BackPropagation
 {
     public partial class Form1 : Form
     {
         NeuralNet neuro = new NeuralNet(4, 10, 1);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void trainbtn_Click(object sender, EventArgs e)
+        {
+            // reading training data from file
+            using (var reader = new StreamReader("../../../trainingset.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var records = csv.GetRecords<TrainingData>().ToList();
+
+                // training starts here
+                for(int x = 0; x < Convert.ToInt32(trainepochin.Text); x++)
+                {
+                    foreach (var record in records)
+                    {
+                        neuro.setInputs(0, record.variance);
+                        neuro.setInputs(1, record.skewness);
+                        neuro.setInputs(2, record.curtosis);
+                        neuro.setInputs(3, record.entropy);
+                        neuro.setDesiredOutput(0, record.classification);
+                        neuro.learn();
+                    }
+                }
+                Console.WriteLine("don dokodone");
+            }
         }
     }
 }
